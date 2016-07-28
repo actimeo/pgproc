9f84b2a55373597dc4120ea118dd798a971094b3
pmartin <contact@elol.fr>
2016-07-28 16:49:17 +0200

    Script to create/update database locally

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,6 @@
 # Create database
 psql -c "create user $DBUSER password '$DBPASS'" -U postgres
 psql -c "CREATE DATABASE $DBNAME WITH ENCODING='UTF8' owner=$DBUSER" -U postgres
-PGPASSWORD=$DBPASS psql $DBNAME -c "CREATE SCHEMA pgcrypto AUTHORIZATION $DBUSER;"
-psql $DBNAME -c "CREATE EXTENSION pgcrypto WITH SCHEMA pgcrypto;" -U postgres
 
 FILES="src/sql/pgprocedures.sql src/plpgsql/all.sql tests/tests.sql"
 # Install schemas
---
+++ scripts/update.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# to be run locally, as postgres user
+
+function err {
+  echo 'ERROR: '$1;
+  exit 1;
+}
+
+# you can use: sudo su postgres -c ./scripts/update.sh
+[ $(whoami) = 'postgres' ] || err "must be postgres (try: sudo su postgres -c $0)"
+
+[ -e config.sh ] && . config.sh || err 'config.sh not found';
+
+psql <<EOF
+DROP DATABASE IF EXISTS $DBNAME;
+CREATE DATABASE $DBNAME WITH ENCODING='UTF8' OWNER=$DBUSER;
+EOF
+
+FILES="src/sql/pgprocedures.sql src/plpgsql/all.sql tests/tests.sql"
+# Install schemas
+(echo 'BEGIN TRANSACTION; ' && cat $FILES && echo 'COMMIT; ' ) |  PGPASSWORD=$DBPASS PGOPTIONS="--client-min-messages=warning" psql -v ON_ERROR_STOP=1 -q -h localhost -U $DBUSER $DBNAME
