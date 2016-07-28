09fe94319632c43fbe2216b035c386848a81c366
pmartin <contact@elol.fr>
2016-07-28 19:20:25 +0200

    [ci] covers constructor/destructor in tests

--- tests/pgproceduresTest.php
+++ tests/pgproceduresTest.php
@@ -17,25 +17,29 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
   private static $pgPass;
   private static $pgDatabase;
 
-  public static function setUpBeforeClass() {
+  protected function setUp() {
 
     // Get connection params
     global $pg_host, $pg_user, $pg_pass, $pg_database;
-    self::$pgHost = $pg_host;
-    self::$pgUser = $pg_user;
-    self::$pgPass = $pg_pass;
-    self::$pgDatabase = $pg_database;
-    self::assertNotNull(self::$pgHost);
-    self::assertNotNull(self::$pgUser);
-    self::assertNotNull(self::$pgPass);
-    self::assertNotNull(self::$pgDatabase);
+    $this->pgHost = $pg_host;
+    $this->pgUser = $pg_user;
+    $this->pgPass = $pg_pass;
+    $this->pgDatabase = $pg_database;
+    $this->assertNotNull($this->pgHost);
+    $this->assertNotNull($this->pgUser);
+    $this->assertNotNull($this->pgPass);
+    $this->assertNotNull($this->pgDatabase);
     
     // Create object
-    self::$base = new PgProcedures (self::$pgHost, self::$pgUser, self::$pgPass, self::$pgDatabase);
-    self::assertNotNull(self::$base);    
+    $this->base = new PgProcedures ($this->pgHost, $this->pgUser, $this->pgPass, $this->pgDatabase);
+    $this->assertNotNull($this->base);    
 
   }
 
+  protected function tearDown() {
+    unset($this->base);
+  }
+  
   /*********
    * TESTS *
    *********/
@@ -44,108 +48,108 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    * Return values
    */
   public function testReturnsInteger() {
-    $res = self::$base->pgtests->test_returns_integer();
+    $res = $this->base->pgtests->test_returns_integer();
     $this->assertSame($res, 42);
   }
 
   public function testReturnsIntegerAsString() {
-    $res = self::$base->pgtests->test_returns_integer_as_string();
+    $res = $this->base->pgtests->test_returns_integer_as_string();
     $this->assertSame($res, '42');
   }
 
   public function testReturnsString() {
-    $res = self::$base->pgtests->test_returns_string();
+    $res = $this->base->pgtests->test_returns_string();
     $this->assertSame($res, 'hello');
   }
   
   public function testReturnsNumeric() {
-    $res = self::$base->pgtests->test_returns_numeric();
+    $res = $this->base->pgtests->test_returns_numeric();
     $this->assertEquals($res, 3.14159, '', 0.00001);
   }
   
   public function testReturnsReal() {
-    $res = self::$base->pgtests->test_returns_real();
+    $res = $this->base->pgtests->test_returns_real();
     $this->assertEquals($res, 3.14, '', 0.00001);
   }
 
   public function testReturnsBoolTrue() {
-    $res = self::$base->pgtests->test_returns_bool_true();
+    $res = $this->base->pgtests->test_returns_bool_true();
     $this->assertSame($res, true);
   }
 
   public function testReturnsBoolFalse() {
-    $res = self::$base->pgtests->test_returns_bool_false();
+    $res = $this->base->pgtests->test_returns_bool_false();
     $this->assertSame($res, false);
   }
 
   public function testReturnsDate() {
-    self::$base->set_date_return_format("d/m/Y");
-    $res = self::$base->pgtests->test_returns_date();
+    $this->base->set_date_return_format("d/m/Y");
+    $res = $this->base->pgtests->test_returns_date();
     $this->assertRegExp('|^\d\d/\d\d/\d\d\d\d$|', $res);
 
-    self::$base->set_date_return_format("Y-m-d");
-    $res = self::$base->pgtests->test_returns_date();
+    $this->base->set_date_return_format("Y-m-d");
+    $res = $this->base->pgtests->test_returns_date();
     $this->assertRegExp('|^\d\d\d\d-\d\d-\d\d$|', $res);
   }
   
   public function testReturnsInfinityDate() {
-    $res = self::$base->pgtests->test_returns_infinity_date();
+    $res = $this->base->pgtests->test_returns_infinity_date();
     $this->assertNull($res);
   }
   
   public function testReturnsMinusInfinityDate() {
-    $res = self::$base->pgtests->test_returns_minus_infinity_date();
+    $res = $this->base->pgtests->test_returns_minus_infinity_date();
     $this->assertNull($res);
   }
   
   public function testReturns64Date() {
-    self::$base->set_date_return_format("d/m/Y");
-    $res = self::$base->pgtests->test_returns_64bits_date();
+    $this->base->set_date_return_format("d/m/Y");
+    $res = $this->base->pgtests->test_returns_64bits_date();
   }
 
   public function testReturnsTimestamp() {
-    self::$base->set_timestamp_return_format("d/m/Y H:i:s");
-    $res = self::$base->pgtests->test_returns_timestamp();
+    $this->base->set_timestamp_return_format("d/m/Y H:i:s");
+    $res = $this->base->pgtests->test_returns_timestamp();
     $this->assertRegExp('|^\d\d/\d\d/\d\d\d\d \d\d:\d\d:\d\d$|', $res);
 
-    self::$base->set_timestamp_return_format("Y-m-d H:i");
-    $res = self::$base->pgtests->test_returns_timestamp();
+    $this->base->set_timestamp_return_format("Y-m-d H:i");
+    $res = $this->base->pgtests->test_returns_timestamp();
     $this->assertRegExp('|^\d\d\d\d-\d\d-\d\d \d\d:\d\d$|', $res);
   }
 
   public function testReturnsTime() {
-    self::$base->set_time_return_format("H:i:s");
-    $res = self::$base->pgtests->test_returns_time();
+    $this->base->set_time_return_format("H:i:s");
+    $res = $this->base->pgtests->test_returns_time();
     $this->assertRegExp('|^\d\d:\d\d:\d\d$|', $res);
 
-    self::$base->set_time_return_format("H:i");
-    $res = self::$base->pgtests->test_returns_time();
+    $this->base->set_time_return_format("H:i");
+    $res = $this->base->pgtests->test_returns_time();
     $this->assertRegExp('|^\d\d:\d\d$|', $res);
   }
 
   public function testReturnsComposite() {
-    $res = self::$base->pgtests->test_returns_composite();
+    $res = $this->base->pgtests->test_returns_composite();
     $this->assertSame(array('a'=> 1, 'b'=> 'hello'), $res);
   }
 
   public function testReturnsSetofComposite() {
-    $res = self::$base->pgtests->test_returns_setof_composite();
+    $res = $this->base->pgtests->test_returns_setof_composite();
     $this->assertSame(array (array('a'=> 1, 'b'=> 'hello'), array('a'=> 2, 'b'=> 'bye')), $res);
   }
 
 
   public function testReturnsEnum() {
-    $res = self::$base->pgtests->test_returns_enum();
+    $res = $this->base->pgtests->test_returns_enum();
     $this->assertSame('val1', $res);
   }
 
   public function testReturnsEnumArray() {
-    $res = self::$base->pgtests->test_returns_enum_array();
+    $res = $this->base->pgtests->test_returns_enum_array();
     $this->assertSame(array('val1', 'val2'), $res);
   }
 
   public function testReturnsEnumArrayAsNull() {
-    $res = self::$base->pgtests->test_returns_null_enum_array();
+    $res = $this->base->pgtests->test_returns_null_enum_array();
     $this->assertSame(null, $res);
   }
 
@@ -154,7 +158,7 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    * @expectedException \actimeo\pgproc\PgProcFunctionNotAvailableException
    */
   public function testNotFoundFunction() {
-    $res = self::$base->pgtests->not_found_function();
+    $res = $this->base->pgtests->not_found_function();
   }
 
   /**
@@ -162,14 +166,14 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    * @expectedException \actimeo\pgproc\PgProcFunctionNotAvailableException
    */
   public function testNotFoundHiddenFunction() {
-    $res = self::$base->pgtests->_hidden_function();
+    $res = $this->base->pgtests->_hidden_function();
   }
 
   /**
    * Function in right schema
    */
   public function testRightSchema() {
-    $res = self::$base->pgtests->function_in_tests_schema();
+    $res = $this->base->pgtests->function_in_tests_schema();
     $this->assertTrue($res);
   }
 
@@ -178,14 +182,14 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    * @expectedException \actimeo\pgproc\PgProcFunctionNotAvailableException
    */
   public function testWrongSchema() {
-    $res = self::$base->otherschema->function_in_tests_schema();
+    $res = $this->base->otherschema->function_in_tests_schema();
   }
 
   /**
    * @expectedException \actimeo\pgproc\PgProcException
    */
   public function testFunctionRaisingException() {
-    $res = self::$base->pgtests->function_raising_exception();
+    $res = $this->base->pgtests->function_raising_exception();
   }
 
   /*
@@ -193,81 +197,81 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    */
   public function testIncrementedInteger() {
     $n = 4;
-    $res = self::$base->pgtests->test_returns_incremented_integer($n);
+    $res = $this->base->pgtests->test_returns_incremented_integer($n);
     $this->assertSame($n + 1, $res);
   }
 
   public function testIncrementedNumeric() {
     $n = 3.14;
-    $res = self::$base->pgtests->test_returns_incremented_numeric($n);
+    $res = $this->base->pgtests->test_returns_incremented_numeric($n);
     $this->assertEquals($n + 1.5, $res, '', 0.00001);
   }
 
   public function testIncrementedReal() {
     $n = 1.414;
-    $res = self::$base->pgtests->test_returns_incremented_real($n);
+    $res = $this->base->pgtests->test_returns_incremented_real($n);
     $this->assertEquals($n + 1.42, $res, '', 0.00001);
   }
 
   public function testCatString() {
     $s = 'hello';
-    $res = self::$base->pgtests->test_returns_cat_string($s);
+    $res = $this->base->pgtests->test_returns_cat_string($s);
     $this->assertSame($s . '.', $res);
   }
 
   public function testSameBool() {
-    $res = self::$base->pgtests->test_returns_same_bool(true);
+    $res = $this->base->pgtests->test_returns_same_bool(true);
     $this->assertSame($res, true);
 
-    $res = self::$base->pgtests->test_returns_same_bool(false);
+    $res = $this->base->pgtests->test_returns_same_bool(false);
     $this->assertSame($res, false);
   }
 
   public function testSameDate() {
-    self::$base->set_date_return_format("d/m/Y");
-    self::$base->set_date_arg_format("%Y-%m-%d");
-    $res = self::$base->pgtests->test_returns_same_date('2015-05-04');
+    $this->base->set_date_return_format("d/m/Y");
+    $this->base->set_date_arg_format("%Y-%m-%d");
+    $res = $this->base->pgtests->test_returns_same_date('2015-05-04');
     $this->assertSame($res, '04/05/2015');
 
-    self::$base->set_date_return_format("Y-m-d");
-    self::$base->set_date_arg_format("%d/%m/%Y");
-    $res = self::$base->pgtests->test_returns_same_date('07/11/2015');
+    $this->base->set_date_return_format("Y-m-d");
+    $this->base->set_date_arg_format("%d/%m/%Y");
+    $res = $this->base->pgtests->test_returns_same_date('07/11/2015');
     $this->assertSame($res, '2015-11-07');
   }
 
   public function testSameTimestamp() {
-    self::$base->set_timestamp_return_format("d/m/Y H:i:s");
-    self::$base->set_timestamp_arg_format("%Y-%m-%d %l:%M %p");
-    $res = self::$base->pgtests->test_returns_same_timestamp('2015-05-04 02:25 PM');
+    $this->base->set_timestamp_return_format("d/m/Y H:i:s");
+    $this->base->set_timestamp_arg_format("%Y-%m-%d %l:%M %p");
+    $res = $this->base->pgtests->test_returns_same_timestamp('2015-05-04 02:25 PM');
     $this->assertSame($res, '04/05/2015 14:25:00');
 
-    self::$base->set_timestamp_return_format("Y-m-d h:i:s A");
-    self::$base->set_timestamp_arg_format("%d/%m/%Y %H:%M");
-    $res = self::$base->pgtests->test_returns_same_timestamp('04/05/2015 14:25');
+    $this->base->set_timestamp_return_format("Y-m-d h:i:s A");
+    $this->base->set_timestamp_arg_format("%d/%m/%Y %H:%M");
+    $res = $this->base->pgtests->test_returns_same_timestamp('04/05/2015 14:25');
     $this->assertSame($res, '2015-05-04 02:25:00 PM');
   }
 
   public function testSameTime() {
-    self::$base->set_time_return_format("H:i:s");
-    self::$base->set_time_arg_format("%l:%M %p");
-    $res = self::$base->pgtests->test_returns_same_time('02:25 PM');
+    $this->base->set_time_return_format("H:i:s");
+    $this->base->set_time_arg_format("%l:%M %p");
+    $res = $this->base->pgtests->test_returns_same_time('02:25 PM');
     $this->assertSame($res, '14:25:00');
 
-    self::$base->set_time_return_format("h:i:s A");
-    self::$base->set_time_arg_format("%H:%M");
-    $res = self::$base->pgtests->test_returns_same_time('14:25');
+    $this->base->set_time_return_format("h:i:s A");
+    $this->base->set_time_arg_format("%H:%M");
+    $res = $this->base->pgtests->test_returns_same_time('14:25');
     $this->assertSame($res, '02:25:00 PM');
   }
 
   public function testIntegerArrayArg() {
     $in = array (1, 2, 3, 4);
-    $out = self::$base->pgtests->test_integer_array_arg($in);
+    $out = $this->base->pgtests->test_integer_array_arg($in);
     $this->assertSame($out, $in);
   }
 
   public function testVarcharArrayArg() {
     $in = array ('a', 'b', 'c');
-    $out = self::$base->pgtests->test_varchar_array_arg($in);
+    $out = $this->base->pgtests->test_varchar_array_arg($in);
     $this->assertSame($out, $in);
   }
 
@@ -276,7 +280,7 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    */
   public function testCount() {
     $in = array (1, 2, 3, 4);
-    $out = self::$base->pgtests->test_integer_array_arg($in, PgProcedures::count());
+    $out = $this->base->pgtests->test_integer_array_arg($in, PgProcedures::count());
     $this->assertSame($out, count($in));
   }
 
@@ -285,11 +289,11 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    */
   public function testOrder() {
     $in = array (1, 3, 2, 4);
-    $out = self::$base->pgtests->test_integer_array_arg($in, 
+    $out = $this->base->pgtests->test_integer_array_arg($in, 
 						      PgProcedures::order('test_integer_array_arg', 'DESC'));
     $this->assertSame(array(4, 3, 2, 1), $out);
 
-    $out = self::$base->pgtests->test_integer_array_arg($in, 
+    $out = $this->base->pgtests->test_integer_array_arg($in, 
 						      PgProcedures::order('test_integer_array_arg', 'ASC'));
     $this->assertSame(array(1, 2, 3, 4), $out);
   }
@@ -299,7 +303,7 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    */
   public function testLimit() {
     $in = array (1, 3, 2, 4);
-    $out = self::$base->pgtests->test_integer_array_arg($in, 
+    $out = $this->base->pgtests->test_integer_array_arg($in, 
 						      PgProcedures::limit(2));
     $this->assertSame(array(1, 3), $out);
   }
@@ -309,7 +313,7 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    */
   public function testLimitOffset() {
     $in = array (1, 3, 2, 4);
-    $out = self::$base->pgtests->test_integer_array_arg($in, 
+    $out = $this->base->pgtests->test_integer_array_arg($in, 
 						      PgProcedures::limit(2, 1));
     $this->assertSame(array(3, 2), $out);
   }
@@ -319,7 +323,7 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    */
   public function testDistinct() {
     $in = array (1, 3, 2, 3, 4);
-    $out = self::$base->pgtests->test_integer_array_arg($in, 
+    $out = $this->base->pgtests->test_integer_array_arg($in, 
 						      PgProcedures::distinct(),
 						      PgProcedures::order('test_integer_array_arg'));
     $this->assertSame(array(1, 2, 3, 4), $out);
@@ -329,38 +333,38 @@ class pgproceduresTest extends PHPUnit_Framework_TestCase {
    * client encoding 
    */
   public function testGetClientEncoding() {
-    $enc = self::$base->get_client_encoding();
+    $enc = $this->base->get_client_encoding();
     $this->assertEquals($enc, 'UTF8');
     
-    $utf8string = self::$base->pgtests->test_returns_accented_string();
+    $utf8string = $this->base->pgtests->test_returns_accented_string();
     $this->assertEquals($utf8string, 'héllo'); // Takes care this current file is utf-8 encoded
   }
 
   public function testSetClientEncoding() {
-    $enc = self::$base->get_client_encoding();
+    $enc = $this->base->get_client_encoding();
     $this->assertEquals($enc, 'UTF8');
-    self::$base->set_client_encoding('ISO-8859-1');
-    $isoString = self::$base->pgtests->test_returns_accented_string();
-    self::$base->set_client_encoding($enc);
+    $this->base->set_client_encoding('ISO-8859-1');
+    $isoString = $this->base->pgtests->test_returns_accented_string();
+    $this->base->set_client_encoding($enc);
     $this->assertEquals($isoString, utf8_decode('héllo')); // Takes care this current file is utf-8 encoded
   }
 
   public function testEnumArg() {
-    $val = self::$base->pgtests->test_enum_arg('val1');
+    $val = $this->base->pgtests->test_enum_arg('val1');
     $this->assertEquals('val1', $val); // Takes care this current file is utf-8 encoded
   }
 
   public function testEnumArrayArg() {
-    $val = self::$base->pgtests->test_enum_array_arg(array('val1', 'val2'));
+    $val = $this->base->pgtests->test_enum_array_arg(array('val1', 'val2'));
     $this->assertEquals($val, array('val1', 'val2')); // Takes care this current file is utf-8 encoded
   }
 
   public function testEnumEmptyArrayArg() {
-    $val = self::$base->pgtests->test_enum_array_arg(array());
+    $val = $this->base->pgtests->test_enum_array_arg(array());
     $this->assertEquals($val, null); // Takes care this current file is utf-8 encoded
   }
 
   public function testReturnsEmptyArray() {
-    $val = self::$base->pgtests->test_returns_empty_array();
+    $val = $this->base->pgtests->test_returns_empty_array();
   }
 }
